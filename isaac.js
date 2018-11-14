64c4ae23650f9a42f06076ed69302f280a1d4ad7
antoinenicolleau <34570446+antoinenicolleau@users.noreply.github.com>
2018-11-14 10:47:25 +0100

    2e envoi

--- isaac.js
+++ isaac.js
@@ -1,4 +1,11 @@
 const axios = require('axios');
+const asciify = require('asciify-image')
+
+const options = {
+    fit: 'box',
+    width: 20,
+    height: 5cd,
+}
 
 let data = {
     bossName : [],
@@ -8,6 +15,11 @@ let data = {
 
 }
 
+let api_boss = 'https://isaac.jamesmcfadden.co.uk/api/v1/boss?page='
+let api_char = 'https://isaac.jamesmcfadden.co.uk/api/v1/character'
+let api_item = 'http://isaac.jamesmcfadden.co.uk/api/v1/item?page='
+let api_monster = 'https://isaac.jamesmcfadden.co.uk/api/v1/monster?page='
+
 
 function getDataName (values, nbData, obj){
     for (let name of values[nbData].data.data) {
@@ -16,22 +28,22 @@ function getDataName (values, nbData, obj){
 
 }
 
-let boss =  axios.get('https://isaac.jamesmcfadden.co.uk/api/v1/boss?page=1')
-let boss2 = axios.get('https://isaac.jamesmcfadden.co.uk/api/v1/boss?page=2')
-let personnage = axios.get('https://isaac.jamesmcfadden.co.uk/api/v1/character')
-let item1 = axios.get('http://isaac.jamesmcfadden.co.uk/api/v1/item?page=1')
-let item2 = axios.get('http://isaac.jamesmcfadden.co.uk/api/v1/item?page=2')
-let item3 = axios.get('http://isaac.jamesmcfadden.co.uk/api/v1/item?page=3')
-let item4 = axios.get('http://isaac.jamesmcfadden.co.uk/api/v1/item?page=4')
-let item5 = axios.get('http://isaac.jamesmcfadden.co.uk/api/v1/item?page=5')
-let item6 = axios.get('http://isaac.jamesmcfadden.co.uk/api/v1/item?page=6')
-let item7 = axios.get('http://isaac.jamesmcfadden.co.uk/api/v1/item?page=7')
-let item8 = axios.get('http://isaac.jamesmcfadden.co.uk/api/v1/item?page=8')
-let item9 = axios.get('http://isaac.jamesmcfadden.co.uk/api/v1/item?page=9')
-let monster1 = axios.get('https://isaac.jamesmcfadden.co.uk/api/v1/monster?page=1')
-let monster2 = axios.get('https://isaac.jamesmcfadden.co.uk/api/v1/monster?page=2')
-let monster3 = axios.get('https://isaac.jamesmcfadden.co.uk/api/v1/monster?page=3')
-let monster4 = axios.get('https://isaac.jamesmcfadden.co.uk/api/v1/monster?page=4')
+let boss =  axios.get(api_boss + 1 )
+let boss2 = axios.get(api_boss + 2)
+let personnage = axios.get(api_char)
+let item1 = axios.get(api_item + 1)
+let item2 = axios.get(api_item + 2)
+let item3 = axios.get(api_item + 3)
+let item4 = axios.get(api_item + 4)
+let item5 = axios.get(api_item + 5)
+let item6 = axios.get(api_item + 6)
+let item7 = axios.get(api_item + 7)
+let item8 = axios.get(api_item + 8)
+let item9 = axios.get(api_item + 9)
+let monster1 = axios.get(api_monster + 1)
+let monster2 = axios.get(api_monster + 2)
+let monster3 = axios.get(api_monster + 3)
+let monster4 = axios.get(api_monster + 4)
 
 
 Promise.all([boss, boss2, personnage, item1, item2, item3, item4, item5, item6, item7, item8, item9, monster1, monster2, monster3, monster4])
@@ -39,20 +51,23 @@ Promise.all([boss, boss2, personnage, item1, item2, item3, item4, item5, item6, 
         getDataName(values, 0, data.bossName)
         getDataName(values, 1, data.bossName)
         getDataName(values, 2, data.characterName)
-        getDataName(values, 3, data.itemName)
-        getDataName(values, 4, data.itemName)
-        getDataName(values, 5, data.itemName)
-        getDataName(values, 6, data.itemName)
-        getDataName(values, 7, data.itemName)
-        getDataName(values, 8, data.itemName)
-        getDataName(values, 9, data.itemName)
-        getDataName(values, 10, data.itemName)
-        getDataName(values, 11, data.itemName)
+        for(let i=3; i<12; i++){
+        getDataName(values, i, data.itemName)
+        }
+        for(let i = 11; i<16 ; i++){} 
         getDataName(values, 12, data.monsterName)
         getDataName(values, 13, data.monsterName)
         getDataName(values, 14, data.monsterName)
         getDataName(values, 15, data.monsterName)
         console.log(data)
+        }
+        
+    )
+
+    asciify('img/characters/isaac.png', options, function(err,asciified){
+        if (err) throw err;
+        
+        console.log(asciified);
     })
 
 // asciify pour les sprites
