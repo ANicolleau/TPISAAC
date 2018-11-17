d3c6d27f66eb31c34c2b2e611cdf677db1781649
VernerBoisson <34570568+VernerBoisson@users.noreply.github.com>
2018-11-17 19:51:10 +0100

    bd

--- isaac.js
+++ isaac.js
@@ -1,278 +1,153 @@
 #!/usr/bin/node node
 
 /*
-Modules:
+Modules :
 commander
 axios
-sqlite
-path
+sqlite3
+
 */
 
+// VARIABLES
 
-// https://www.npmjs.com/package/commander
-// http://www.sqlitetutorial.net/sqlite-nodejs/connect/
+const commander = require('commander')
+const sqlite3 = require('sqlite3').verbose()
+const axios = require('axios')
+const inquirer = require('inquirer');
+const db_name = "./isaac.db"
+const api_character = 'https://isaac.jamesmcfadden.co.uk/api/v1/character'
+let object = {
+    character_names:[]
+}
 
+// Program
 
-const program = require('commander');
-const sqlite3 = require('sqlite3').verbose();
- 
-program
+commander
   .version('0.1.0')
-  .option('init', 'Initialise les Bases de données')
-  .option('-C, --characters-list', 'Affiche la liste des personnages')
+  .option('-I, --init', 'Initialise le programme')
+  .option('-C, --list-characters', 'Affiche la liste des personnages')
   .parse(process.argv);
  
-if (program.init) init();
-if (program.charactersList) console.log("list perso")
+if (commander.init) Init();
+if (commander.listCharacters) ListCharacters();
 
-const axios = require("axios");
-const url = "https://isaac.jamesmcfadden.co.uk/api/v1/"
-let data = {
-    bosses:[],
-    characters:[],
-    items:[]
-};
 
-const statCharacters={
-"???":{
-health:"B, B, B",
-damage:"3.5*(1.05)",
- tears:"+0",
- shotspeed:"1",
- range:"23.75",
- speed:"1.1",
- luck:"0",
- startingpickup:"None",
- startingitem:"The Poop"
-},Azazel:{
-health:"N, N, N",
-damage:"3.5*(1.50)",
- tears:"+0.5, *1/3",
- shotspeed:"1",
- range:"17.75",
- speed:"1.25",
- luck:"0",
- startingpickup:"The Fool",
- startingitem:"Brimstone, Flight"
-},Cain:{
-health:"R, R",
-damage:"3.5*(1.20)",
- tears:"+0",
- shotspeed:"1",
- range:"17.75",
- speed:"1.3",
- luck:"0",
- startingpickup:"x1 keys, Paper Clip",
- startingitem:"Lucky Foot"
-},Eden:{
-health:"random",
-damage:"3.5*(1.00) +/- 1.00",
- tears:"+/- 0.75",
- shotspeed:"1+/- 0.25",
- range:"23.75+/-5.00",
- speed:"1.0+/-0.15",
- luck:"0+/-1",
- startingpickup:"x0-2 bombs, x0-1 keys, x0-5 coins, x0-1 trinket, pill, or card",
- startingitem:"Random active item, Random passive item"
-},Eve:{
-health:"R, R",
-damage:"3.5*(0.75)(*1.00atR)",
- tears:"+0",
- shotspeed:"1",
- range:"23.75",
- speed:"1.23",
- luck:"0",
- startingpickup:"None",
- startingitem:"Whore of Babylon, Dead Bird, Razor Blade"
-},Isaac:{
- health:"R, R, R",
-damage:"3.5*(1.00)",
- tears:"+0",
- shotspeed:"1",
- range:"23.75",
- speed:"1.0",
- luck:"0",
- startingpickup:"x1 bombs",
- startingitem:"The D6"
-},Judas:{
-health:"R",
-damage:"3.5*(1.35)",
- tears:"+0",
- shotspeed:"1",
- range:"23.75",
- speed:"1.0",
- luck:"0",
- startingpickup:"x3 coins",
- startingitem:"The Book of Belial"
-},Keeper:{
-health:"G, G",
-damage:"3.5*(1.20)",
- tears:"+18",
- shotspeed:"1",
- range:"23.75",
- speed:"0.85",
- luck:"-2",
- startingpickup:"x1 bombs, x1 coins, Store Key",
- startingitem:"Triple Shot, Wooden Nickel"
-},Lazarus:{
-health:"R, N, N",
-damage:"3.5*(1.00)",
- tears:"+0",
- shotspeed:"1",
- range:"23.75",
- speed:"1.0",
- luck:"0",
- startingpickup:"None",
- startingitem:"Anemic"
-},Lilith:{
-health:"R",
-damage:"3.5*(1.35)",
- tears:"+0",
- shotspeed:"1",
- range:"23.75",
- speed:"1.0",
- luck:"0",
- startingpickup:"x3 coins",
- startingitem:"Blindfolded, Incubus, Cambion Conception, Box of Friends"
-},Magdalene:{
-health:"R, R, R, R",
-damage:"3.5*(1.00)",
- tears:"+0",
- shotspeed:"1",
- range:"23.75",
- speed:"0.85",
- luck:"0",
- startingpickup:"x1 Speed Up pill",
- startingitem:"Yum Heart"
-},Samson:{
-health:"R, R, R",
-damage:"3.5*(1.35)",
- tears:"-0.05",
- shotspeed:"1.31",
- range:"28.75",
- speed:"1.1",
- luck:"0",
- startingpickup:"Child's Heart",
- startingitem:"Bloody Lust"
-},"The Lost":{
-health:"No health",
-damage:"3.5*(1.00)",
- tears:"+0",
- shotspeed:"1",
- range:"23.75",
- speed:"1.0",
- luck:"0",
- startingpickup:"x1 coins",
- startingitem:"Flight, Spectral tears, D4, Holy Mantle"
-}
+// Functions
+
+function Init(){
+    let database = OpenDB()
+
+    axios.get(api_character).then((response)=>{
+        for(let i = 0; i<response.data.data.length; i++){
+            object.character_names.push(response.data.data[i].name)
+        }
+    }).then(()=>{
+        CreateSqlCharacters(database)
+    })
 }
 
-const boss1 = axios.get(url+'boss?page=1')
-const boss2 = axios.get(url+'boss?page=2')
-const character = axios.get(url+'character')
-const item1 = axios.get(url+'item?page=1');
-const item2 = axios.get(url+'item?page=2');
-const item3 = axios.get(url+'item?page=3');
-const item4 = axios.get(url+'item?page=4');
-const item5 = axios.get(url+'item?page=5');
-const item6 = axios.get(url+'item?page=6');
-const item7 = axios.get(url+'item?page=7');
-const item8 = axios.get(url+'item?page=8');
-const item9 = axios.get(url+'item?page=9');
-
-
-const path = require('path')
-const dbPath = path.resolve(__dirname, 'toto.db')
-const db = new sqlite3.Database(dbPath)
-
-
-Promise.all([boss1, boss2, character, item1, item2, item3, item4, item5, item6, item7, item8, item9 ])
-.then(function (response){
-    getData(data.bosses, response[0])
-    getData(data.bosses, response[1])
-    getData(data.characters, response[2])
-    for(let i=3; i<12; i++)
-        getData(data.items, response[i])
-}).catch(function (response){
-
-})
-
-function getData(variable ,response){
-    for(let tmp of response.data.data){
-        variable.push(tmp.name) 
-    }
-}
-
-
-
-function createDB(){
-    let sql = `CREATE TABLE characters (
-        character_id integer PRIMARY KEY,
-        name text NOT NULL
-        );`;
-    try{
-        db.run(sql)
-    }catch(e){
-        console.log(e.message)
-    }
-}
-
-
-function insertDB(name){
-    let sql = `INSERT INTO characters (name)
-        VALUES
-        ('`+name+`');`;
-    try{
-        db.run(sql)
-    }catch(e){
-        console.log(e.message)
-    }
-}
-
-function init(){
-    createDB()
-    data.name.forEach(insertDB(this))
-    db.close()
-}
-
-function afficherCharacter(){
-    let sql3 = `SELECT
-        character_id,
-        name
-        FROM
-        characters;`
-
-    db.all(sql3, [], (err, rows) => {
+function CreateSqlCharacters(database){
+    let sqlcreate = `CREATE TABLE characters (
+        characters_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name text NOT NULL,
+        health text,
+        damage text,
+        tears text,
+        shotspeed text,
+        range text,
+        speed text,
+        startingpickup text,
+        startingitem text
+    )`
+    
+    database.run(sqlcreate, (err) => {
         if (err) {
-        throw err;
+          throw err;
+        }
+        console.log("Create")
+        InsertSqlCharacters(database)
+    });
+}
+
+
+function InsertSqlCharacters(database){
+    let placeholders = object.character_names.map((value) => '(?)').join(',');
+    let sqlinsert = `INSERT INTO characters (
+        name)
+       VALUES`+ placeholders
+
+    database.run(sqlinsert, object.character_names, (err) => {
+        if (err) {
+          throw err;
+        }
+        console.log("Insert")
+        UpdateSqlCharacters(database)
+        
+    });
+}
+
+function UpdateSqlCharacters(database){
+    const stat = require('./stat.json')
+
+    for(let i in stat){
+        let sqlupdate = `UPDATE characters
+            SET health = "`+stat[i].health+`",
+                damage = "`+stat[i].damage+`",
+                tears = "`+stat[i].tears+`",
+                shotspeed = "`+stat[i].shotspeed+`",
+                range = "`+stat[i].range+`",
+                speed = "`+stat[i].speed+`",
+                startingpickup = "`+stat[i].startingpickup+`",
+                startingitem = "`+stat[i].startingitem+`"
+            WHERE
+                name = "`+i+`";`
+    
+        database.run(sqlupdate, (err) => {
+            if (err) {
+            throw err;
+            }
+            console.log("Update")
+        });
+    }
+    CloseDB(database)
+}
+
+
+function ListCharacters(){
+    let database = OpenDB()
+
+    let sqlselect = `SELECT * FROM characters`
+
+    database.all(sqlselect, [], (err, rows) => {
+        if (err) {
+          throw err;
         }
         rows.forEach((row) => {
-        console.log(row);
+          console.log(row.name);
         });
     });
 
+    CloseDB(database)
+    
 }
 
+function CloseDB(database){
+    database.close((err) => {
+        if (err) {
+          return console.error(err.message);
+        }
+        //console.log('Close the database connection.');
+    });
+}
 
+function OpenDB(){
+    let database = new sqlite3.Database(db_name, (err) => {
+        if (err) {
+          console.error(err.message);
+        }
+        //console.log('Connected to the isaac database.');
+    });
+    
+    return database
+}
 
-/*
-axios.get(url+'character')
-.then(function (response){
-    test.character=response.data.data
-    console.log(test)
-}).catch(function (response){
-    console.log(response)
-});
-*/
-
-/*
-Arguments
-
-npm install commander --save
-
-
-*/
-
-/* Intéraction avec l'utilisateur : https://github.com/SBoudrias/Inquirer.js/ */
-
-/* https://www.npmjs.com/package/sqlite SQLitei */ 
